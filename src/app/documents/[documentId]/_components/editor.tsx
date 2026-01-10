b667519fa099b653e31308ed1981a97d16f050f6
Ahmed Fahmy <ahmedmewans2030@gmail.com>
2026-01-10 10:54:14 +0200

    feat(image)

--- src/app/documents/[documentId]/_components/editor.tsx
+++ src/app/documents/[documentId]/_components/editor.tsx
@@ -1,7 +1,11 @@
 "use client";
 
+import { TaskItem, TaskList } from "@tiptap/extension-list";
+import { TableKit } from "@tiptap/extension-table";
 import { EditorContent, useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+// import Image from "@tiptap/extension-image";
+import ImageResize from "tiptap-extension-resize-image";
 
 export const Editor = () => {
   const editor = useEditor({
@@ -12,10 +16,29 @@ export const Editor = () => {
           "prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text",
       },
     },
-    extensions: [StarterKit],
-    content: "<p>Hello World! 🌎️</p>",
+    extensions: [
+      StarterKit,
+      TaskList,
+      // Image,
+      ImageResize,
+      TaskItem.configure({
+        nested: true,
+      }),
+      TableKit.configure({
+        table: { resizable: true },
+      }),
+    ],
+    content: `
+        <p>This is a basic example of implementing images. Drag to re-order.</p>
+        <img src="https://placehold.co/600x400" />
+        <img src="https://placehold.co/800x400" />
+      `,
     immediatelyRender: false,
   });
+
+  if (!editor) {
+    return null;
+  }
 
   return (
     <div className="size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible">
